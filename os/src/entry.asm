391feaf10b293a6cc670e4e4088c285bf816aaf2
SFXiang <444051989@qq.com>
2025-04-17 23:41:24 +0800

    添加注释

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -1,11 +1,11 @@
     .section .text.entry
     .globl _start
 _start:
-    la sp, boot_stack_top
+    la sp, boot_stack_top // 栈顶
     call rust_main
 
     .section .bss.stack
-    .globl boot_stack_lower_bound
+    .globl boot_stack_lower_bound // 栈底
 boot_stack_lower_bound:
     .space 4096 * 16
     .globl boot_stack_top
